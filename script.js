785d86ee8c1999ecfc987814e1c707b448b8c59f
Rodrigo Santiago <rs.santiago1997@gmail.com>
2021-02-14 02:51:54 -0300

    adicionado style e score

--- script.js
+++ script.js
@@ -65,19 +65,22 @@ function iniciarJogo() {
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    if (snakeX != food.x || snakeY != food.y) {
-        snake.pop();
-    }else{
-        food.x = Math.floor(Math.random() * 15 + 1) * box
-        food.y = Math.floor(Math.random() * 15 + 1) * box
-    }
-    
     let newHead = {
         x: snakeX,
         y: snakeY
     }
 
     snake.unshift(newHead);
+
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    }else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+        document.getElementById("pontos").innerHTML = (snake.length - 1);
+    }
+    
+    
 }
 
 let jogo = setInterval(iniciarJogo, 100); 
